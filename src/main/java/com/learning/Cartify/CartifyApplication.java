5bb9acb5ee88f9d178a96855daf29d77ca0a3c7c
tek.ninja <ninjatekpro@gmail.com>
2026-06-30 11:24:44 +0530

    Cartify Application

---
+++ src/main/java/com/learning/Cartify/CartifyApplication.java
@@ -0,0 +1,13 @@
+package com.learning.Cartify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/Cartify/CartifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.Cartify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
