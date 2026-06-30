e08fd43dbb7626cca7dbf2908d42867be922d687
tek.ninja <ninjatekpro@gmail.com>
2026-06-30 11:47:59 +0530

    Comment Updated

--- src/main/java/com/learning/Cartify/HomeController.java
+++ src/main/java/com/learning/Cartify/HomeController.java
@@ -2,7 +2,7 @@ package com.learning.Cartify;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-// Comment
+// Hello Controller for the initial welcome message
 @RestController
 public class HomeController {
 
