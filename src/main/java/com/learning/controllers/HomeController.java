649cf69bd1c4840125a3352e1ce27d5daba52e21
tek.ninja <ninjatekpro@gmail.com>
2026-07-02 11:28:53 +0530

    Added ResponseEntity

--- src/main/java/com/learning/controllers/HomeController.java
+++ src/main/java/com/learning/controllers/HomeController.java
@@ -2,6 +2,8 @@ package com.learning.controllers;
 
 import com.learning.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.learning.models.User;
 import com.learning.service.UserService;
@@ -17,16 +19,20 @@ public class HomeController {
     private final UserService userService;
 
     @GetMapping("/api/users")
-    public List<User> getUsers() {
-        return userService.getUsers();
+    public ResponseEntity<List<User>> getUsers() {
+        return new ResponseEntity<>(userService.getUsers(),HttpStatus.OK);
     }
 
     @GetMapping("/api/users/{id}")
-    public User getUserById(@PathVariable Long id) {
-        return userService.getUserById(id);
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        if(user == null)
+            return ResponseEntity.notFound().build();
+        return new ResponseEntity<>(user,HttpStatus.FOUND);
     }
     @PostMapping("/api/users")
-    public List<User> createUser(@RequestBody User user) {
-        return userService.addUser(user);
+    public ResponseEntity<List<User>> createUser(@RequestBody User user) {
+
+        return new ResponseEntity<>(userService.addUser(user),HttpStatus.CREATED);
     }
 }
